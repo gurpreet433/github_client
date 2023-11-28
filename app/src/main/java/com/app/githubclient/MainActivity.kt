731e734f3c7e6b557ae8f3598217b3fbadd0cfa0
GurpreetSudan <gurpreetsudan2094@gmail.com>
2023-11-28 21:57:47 +0530

    searchView working with toasts in home fragment

--- app/src/main/java/com/app/githubclient/MainActivity.kt
+++ app/src/main/java/com/app/githubclient/MainActivity.kt
@@ -2,22 +2,11 @@ package com.app.githubclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.SearchView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String): Boolean {
-                Toast.makeText(this@MainActivity, "No Match found", Toast.LENGTH_LONG).show()
-            }
-            override fun onQueryTextChange(newText: String): Boolean {
-                return false
-            }
-        })
     }
 }
--- app/src/main/java/com/app/githubclient/home/HomeFragment.kt
+++ app/src/main/java/com/app/githubclient/home/HomeFragment.kt
@@ -5,18 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.app.githubclient.R
+import com.app.githubclient.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var binding: FragmentHomeBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(requireContext(), "Search done", Toast.LENGTH_LONG).show()
+                return false
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                Toast.makeText(requireContext(), "Typing...", Toast.LENGTH_LONG).show()
+                return false
+            }
+        })
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        savedInstanceState: Bundle?): View? {
+
+        binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater,
+                                R.layout.fragment_home, container, false)
+        return binding!!.root
     }
 }
